036b6610a814b9217984b070e81e967c9c22ba93
whereisjw <j9399977@gmail.com>
2023-12-09 12:00:00 +0900

    글목록 글상세페이지 get요청

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   writer: string;
   title: string;
   content: string;
@@ -10,26 +11,73 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    writer: '작성자',
+    title: '글제목',
+    content: '글내용',
+    commentCount: 2,
+    likeCount: 9,
+  },
+  {
+    id: 2,
+    writer: 'newjeans_fan',
+    title: '뉴진스 민지',
+    content: '뉴진스 민지의 무대 의상',
+    commentCount: 21,
+    likeCount: 92,
+  },
+  {
+    id: 3,
+    writer: 'newjeans_fan',
+    title: '뉴진스 해린',
+    content: '춤 연습하는 뉴진스 해린',
+    commentCount: 4,
+    likeCount: 5,
+  },
+];
+
+// 1) get /posts
+// 모든 posts를 다 가져온다
+
+// 2) get /posts/:id
+// id에 해당되는 post를 가져온다
+// 예를 들어서 id =1 일 경우 id가 1인 포스트를 가져온다.
+
+// 3 post /posts
+// post생성한다
+
+// 4 put /posts/:id
+// id에 해당하는 포스트를 변경
+
+// 5 delete /posts/:id
+// id에 해당하는 포스트를 삭제
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  // 1) get /posts
+  // 모든 posts를 다 가져온다
   @Get()
-  getPost(): Post {
-    return {
-      writer: 'newjeans_fan',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지2',
-      likeCount: 1000,
-      commentCount: 10,
-    };
+  getPosts() {
+    return posts;
   }
+  // 2) get /posts/:id
+  // id에 해당되는 post를 가져온다
+  // 예를 들어서 id =1 일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    return posts.find((post) => post.id === Number(id));
+  }
+
+  // 3 post /posts
+  // post생성한다
+
+  // 4 put /posts/:id
+  // id에 해당하는 포스트를 변경
+
+  // 5 delete /posts/:id
+  // id에 해당하는 포스트를 삭제
 }
-/* 
-writer:string
-title:string
-content:string
-image:string
-likeCount:number
-commentCount:number
-*/
